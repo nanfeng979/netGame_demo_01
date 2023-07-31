2665e5bbe34c56928f9a707e4cba359dcb9b5b87
nanfeng979 <306338401@qq.com>
2023-07-31 23:29:48 +0800

    增加玩家对象、玩家Prefab、相机简单跟随、鼠标控制左右旋转，键盘控制前后左右移动

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,84 +1,37 @@
-using System;
 using System.Collections;
-using System.IO;
-using System.Net.Sockets;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    private TcpClient client;
-    private StreamReader reader;
-    private StreamWriter writer;
+    public float rotateSpeed = 5.0f;
+    private float mouseX;
 
-    private void Start()
+    public float moveSpeed = 5.0f;
+    private float moveX;
+    private float moveY;
+
+    void Start()
     {
-        client = new TcpClient("localhost", 1234); // 服务器的IP地址和端口号
-        NetworkStream stream = client.GetStream();
-        reader = new StreamReader(stream);
-        writer = new StreamWriter(stream);
-
-        StartCoroutine(ReceivePlayerUpdates());
+        
     }
 
-    private void Update()
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        transform.Translate(moveHorizontal * moveSpeed * Time.deltaTime, 0f, moveVertical * moveSpeed * Time.deltaTime);
-
-        SendPlayerPosition(transform.position.x, transform.position.z);
+        Rotate();
+        Move();
     }
 
-    private IEnumerator ReceivePlayerUpdates()
-    {
-        while (true)
-        {
-            try
-            {
-                string message = reader.ReadLine();
-                if (!string.IsNullOrEmpty(message))
-                {
-                    string[] playerData = message.Split(',');
+    private void Rotate() {
+        mouseX += Input.GetAxis("Mouse X") * rotateSpeed;
+        transform.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
 
-                    if (playerData.Length >= 3)
-                    {
-                        string colorString = playerData[0];
-                        float posX = float.Parse(playerData[1]);
-                        float posZ = float.Parse(playerData[2]);
-
-                        UpdateOtherPlayerPosition(colorString, posX, posZ);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Error receiving player updates: " + e.Message);
-                break;
-            }
-
-            yield return null;
+    private void Move() {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        if (moveX != 0 || moveY != 0) {
+            transform.Translate(moveX * moveSpeed * Time.deltaTime, 0, moveY * moveSpeed * Time.deltaTime);
         }
-    }
-
-    private void SendPlayerPosition(float posX, float posZ)
-    {
-        string message = $"{posX},{posZ}";
-        writer.WriteLine(message);
-        writer.Flush();
-    }
-
-    private void UpdateOtherPlayerPosition(string colorString, float posX, float posZ)
-    {
-        // 在此处更新其他玩家的位置和颜色
-        // ...
-    }
-
-    private void OnDestroy()
-    {
-        writer.Close();
-        reader.Close();
-        client.Close();
     }
 }
